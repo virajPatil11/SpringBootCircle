8010eb97ab3d896c4f68b206cb1b4fdb8d2a7476
Viraj Patil <virajpatilvp7045@gmail.com>
2024-04-30 21:48:43 +0530

    Added one test method

---
+++ src/main/java/com/viraj/SpringcircleApplication.java
@@ -0,0 +1,13 @@
+package com.viraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcircleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viraj/SpringcircleApplicationTests.java
@@ -0,0 +1,20 @@
+package com.viraj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringcircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void test1(){
+		assertEquals(3, 1+2);
+	}
+
+}
